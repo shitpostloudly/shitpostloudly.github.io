9311c79451d0d7f8f6b12437748ccf1b926fa52f
mohan-cao <mohancao@yahoo.com.au>
2018-09-11 19:20:18 +1200

    omfg I had adblocker on that's why

--- src/components/GATracker.js
+++ src/components/GATracker.js
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import GoogleAnalytics from 'react-ga';
 
-GoogleAnalytics.initialize('UA-125467783-1');
-
 export default class GAListener extends React.Component {
   static contextTypes = {
     router: PropTypes.object
   };
 
   componentDidMount() {
+    GoogleAnalytics.initialize('UA-125467783-1', { debug: false });
     this.sendPageView(this.context.router.history.location);
     this.context.router.history.listen(this.sendPageView);
   }
