247e36d2b256e20e209247f467156b0b1f6a546a
mohan-cao <mohancao@yahoo.com.au>
2018-09-17 10:37:18 +1200

    Updated presets

--- src/ShitpostJS/FunnyOnes.js
+++ src/ShitpostJS/FunnyOnes.js
@@ -6,6 +6,7 @@ const funnies = {
   'software-response': 'f4kdx6sp8b',
   'reddit1': 'f4ke6f08ou',
   'terran': 'f4kmkyen9a',
-  'unfortunately': 'f4o11lb97a'
+  'unfortunately': 'f4o11lb97a',
+  'confession': 'f4vb5pgsbp',
 }
 export default funnies
