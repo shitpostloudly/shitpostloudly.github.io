5d7cba8eb009ba659d0b888cf329fecd3848f69d
mohan-cao <mohancao@yahoo.com.au>
2018-11-21 10:48:47 +1300

    Updates

---
+++ precache-manifest.7bd6a6924d00291c7135b501250f950a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55c3516c6a154f3bc5b55925248932e4",
+    "url": "/static/media/fidget.55c3516c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76c1309a2d7a3fc56bdb",
+    "url": "/static/js/main.76c1309a.chunk.js"
+  },
+  {
+    "revision": "6f62e025d30bfbb7f43f",
+    "url": "/static/js/1.6f62e025.chunk.js"
+  },
+  {
+    "revision": "76c1309a2d7a3fc56bdb",
+    "url": "/static/css/main.e68fbe64.chunk.css"
+  },
+  {
+    "revision": "4f34ff7a3daa031903a24c13b7761f40",
+    "url": "/index.html"
+  }
+];
