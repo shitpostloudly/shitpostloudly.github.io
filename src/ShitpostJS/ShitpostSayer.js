56b62c400da7a6aba6a7fbe814b704c7477266e2
mohan-cao <mohancao@yahoo.com.au>
2019-02-17 16:35:19 +1300

    Add travis and fixes. Reverted workaround for autoplay changes
    Fuck off chrome devs

--- src/ShitpostJS/ShitpostSayer.js
+++ src/ShitpostJS/ShitpostSayer.js
@@ -14,6 +14,41 @@ const emojiStripper = (input) => {
   return input.replace(emojiRegex(), '')
 };
 
+const speechUtteranceChunker = function (utt, settings, callback) {
+  if (!(utt instanceof window.SpeechSynthesisUtterance)) { callback(false); return; }
+  settings = settings || {};
+  var chunkLength = (settings && settings.chunkLength) ? settings.chunkLength : 160;
+  var pattRegex = new RegExp('^.{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^.{1,' + chunkLength + '}$|^.{1,' + chunkLength + '} ');
+  var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+  var chunkArr = txt.match(pattRegex);
+
+  if (chunkArr && chunkArr[0] !== undefined && chunkArr[0].length > 2) {
+      var chunk = chunkArr[0];
+      var newUtt = new SpeechSynthesisUtterance(chunk.replace(/^\./,''));
+      for (let x in utt) {
+        newUtt["lang"] = utt["lang"]
+        if (utt.hasOwnProperty(x) && x !== 'text') {
+            newUtt[x] = utt[x];
+        }
+      }
+      newUtt.onend = function () {
+          settings.offset = settings.offset || 0;
+          settings.offset += chunk.length - 1;
+          speechUtteranceChunker(utt, settings, callback);
+      }
+      window.TEMP_UTT= newUtt; //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+      //placing the speak invocation inside a callback fixes ordering and onend issues.
+      setTimeout(function () {
+          speechSynthesis.speak(newUtt);
+      }, 0);
+  } else {
+      //call once all text has been spoken...
+      if (callback !== undefined) {
+          callback(true);
+      }
+  }
+}
+
 export default class ShitpostSayer extends Component {
   constructor(props) {
     super(props)
@@ -62,19 +97,28 @@ export default class ShitpostSayer extends Component {
     const Utterance = this.state.Utterance
     if (!(Synth && Utterance)) return
     Synth.cancel()
-    const shitpost = (this.props.shitpost) ? emojiStripper(this.props.shitpost) : ''
+    let preShitpost = (this.props.shitpost) ? emojiStripper(this.props.shitpost) : ''
+    let rate = Math.min(1.5, Math.max(1, preShitpost.split(' ').length/200))
+    //preShitpost = preShitpost.replace("\"", "").split(/[)(.;](\s+)/g).filter(x => x);
+    //preShitpost.forEach(shitpost => {
+    let shitpost = (preShitpost) ? preShitpost : 'hi'
     const utterance = new Utterance(shitpost)
-    utterance.rate = Math.min(1.5, Math.max(1, shitpost.split(' ').length/200))
+    utterance.rate = rate
     const francDetection = langs.where("3", franc(shitpost))
     if (francDetection) {
-      utterance.lang = francDetection
-      Synth.speak(utterance)
+      utterance.lang = francDetection["1"]
+      speechUtteranceChunker(utterance, {}, (success) => {
+        if (!success) Synth.speak(utterance)
+      })
     } else {
       guessLanguage.detect(shitpost, (code) => {
         utterance.lang = code
-        Synth.speak(utterance)
+        speechUtteranceChunker(utterance, {}, (success) => {
+          if (!success) Synth.speak(utterance)
+        })
       })
     }
+    //})
   }
   render() {
     return null
