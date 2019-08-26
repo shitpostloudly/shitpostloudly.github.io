4a6614e4ebb520c51d9064fd3e4262968c3bb689
mohan cao <deploy@travis-ci.org>
2019-08-26 04:14:12 +0000

    Deploy shitpostloudly/shitpostloudly.github.io to github.com/shitpostloudly/shitpostloudly.github.io.git:master

---
+++ precache-manifest.0021f73ab23de9269aa2be340b9db2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f32bd28f4695d25dfd39915f56db795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9228366fe96f48419e26",
+    "url": "/static/css/main.0be43d02.chunk.css"
+  },
+  {
+    "revision": "163513bc84aad7878f0c",
+    "url": "/static/js/2.1afff821.chunk.js"
+  },
+  {
+    "revision": "9228366fe96f48419e26",
+    "url": "/static/js/main.892e62f2.chunk.js"
+  },
+  {
+    "revision": "b1954c357a0c49a487c2",
+    "url": "/static/js/runtime~main.d27a1c97.js"
+  },
+  {
+    "revision": "e9f2330a7406e279a1dcd746161afd50",
+    "url": "/static/media/fidget.e9f2330a.svg"
+  }
+]);
